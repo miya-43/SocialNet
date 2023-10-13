8ecc5e4d68c5dca0ac5b3916adb9bfac47b97731
miya-43 <miyapriv43@gmail.com>
2023-10-13 11:04:24 +0200

    login page body

--- login/login.php
+++ login/login.php
@@ -1,1 +1,57 @@
+<html>
+    <head>
+        <title> Social Net </title>
+    </head>
+    <style>
 
+    #bar {
+        height:100px;
+        background-color: #ac8cc2;
+        color: white;
+        font-size: 40px;
+    }
+    
+    #card {
+        
+        background-color: white; 
+        width: 800px;
+        height: 400px;
+        margin: auto;
+        margin-top: 150px;
+        padding: 10px;
+        padding-top: 50px;
+        text-align: center; 
+    }
+
+    #text_box {
+
+        height: 40px;
+        width: 300px;
+        border-radius: 4px;
+        border: solid 1px gray;
+        
+    }
+
+
+</style>
+
+    <body style="background-color: #fcfcfc;">
+
+        <div id=bar>
+        SocialNet Signup
+        </div>
+
+        <div id=card>
+        Login <br> <br>
+
+        <input type="text" id="text_box" placeholder="Username"> <br><br>
+        <input type="password" id="text_box" placeholder="Password"> <br><br>
+        <input type="submit" id="submit" value="login">
+
+        </div>
+
+
+
+</body>
+
+</html>
