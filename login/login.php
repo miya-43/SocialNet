b846be59243ea862b1241dc46ddd1c27f16307b7
miya-43 <miyapriv43@gmail.com>
2023-10-13 13:57:24 +0200

    +

--- login/login.php
+++ login/login.php
@@ -20,7 +20,8 @@
         margin-top: 150px;
         padding: 10px;
         padding-top: 50px;
-        text-align: center; 
+        text-align: center;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     }
 
     #text_box {
@@ -29,9 +30,21 @@
         width: 300px;
         border-radius: 4px;
         border: solid 1px gray;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+
         
     }
 
+    #submit_box {
+
+        height: 40px;
+        width: 300px;
+        border-radius: 4px;
+        border: solid 1px gray;
+        background-color: blue;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        
+    }
 
 </style>
 
@@ -46,7 +59,7 @@
 
         <input type="text" id="text_box" placeholder="Username"> <br><br>
         <input type="password" id="text_box" placeholder="Password"> <br><br>
-        <input type="submit" id="submit" value="login">
+        <input type="submit" id="submit_box" value="login">
 
         </div>
 
